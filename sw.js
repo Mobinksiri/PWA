a5053712c9480789a87b6f20124a13f5d1ba9091
Mobin <isthemobin@gmail.com>
2022-06-13 14:40:44 +0430

    fetch service worker

--- sw.js
+++ sw.js
@@ -7,3 +7,7 @@ self.addEventListener("install", (e) => {
 self.addEventListener("activate", (e) => {
    console.log("service worker has been activated");
 });
+
+self.addEventListener("fetch", (e) => {
+   console.log("fetch event", e);
+});
