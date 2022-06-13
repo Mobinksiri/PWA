fe4b88c6b9f776ab4cd06a033607e35ed03b2f61
Mobin <isthemobin@gmail.com>
2022-06-13 13:11:21 +0430

    install and activate service worker.

---
+++ sw.js
@@ -0,0 +1,9 @@
+// install service worker
+self.addEventListener("install", (e) => {
+   console.log("service worker has been installed");
+});
+
+// activate service worker
+self.addEventListener("activate", (e) => {
+   console.log("service worker has been activated");
+});
